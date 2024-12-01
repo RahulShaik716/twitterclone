9620031ff79c02eace18ece1d9de3bbe2b28bd81
Rahul <dadarahulshaik@gmail.com>
2024-11-30 18:30:36 -0700

    router

---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const postsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.db.post.findMany();
+  }),
+});
